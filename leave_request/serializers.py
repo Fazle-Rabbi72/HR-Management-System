f06d36d2b5be1a2bd01498451849fa913fc92523
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-02-02 14:09:23 -0600

    updated

--- leave_request/serializers.py
+++ leave_request/serializers.py
@@ -3,7 +3,8 @@ from .models import Leave
 
 class LeaveSerializer(serializers.ModelSerializer):
     total_days = serializers.ReadOnlyField()
+    employee_name = serializers.CharField(source="employee.user.get_full_name", read_only=True)
 
     class Meta:
         model = Leave
-        fields = ['id', 'employee', 'start_date', 'end_date', 'reason', 'status', 'applied_on', 'total_days']
+        fields = ['id', 'employee','employee_name','start_date', 'end_date', 'reason', 'status', 'applied_on', 'total_days']
