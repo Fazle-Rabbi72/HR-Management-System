91b48cabe8feae671867aadaf9eb4ff4a9f41e96
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-02-02 10:35:06 -0600

    update for authentication

--- leave_request/views.py
+++ leave_request/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from .models import Leave
 from .serializers import LeaveSerializer
 
 class LeaveViewSet(viewsets.ModelViewSet):
-    serializer_class = LeaveSerializer
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+    serializer_class = LeaveSerializer
 
     def get_queryset(self):
         return Leave.objects.filter(employee=self.request.user.employee)
