46f7e4ca4af1d290488647d7e4b15cd9d9d7ceaf
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2025-03-04 09:29:22 -0600

    update attendance

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -3,10 +3,11 @@ from .models import Attendance
 
 class AttendanceSerializer(serializers.ModelSerializer):
     total_working_hours = serializers.SerializerMethodField()
+    employee_name = serializers.CharField(source="employee.user.get_full_name", read_only=True)
 
     class Meta:
         model = Attendance
-        fields = ['id', 'employee', 'date', 'status', 'punch_in', 'punch_out', 'total_working_hours']
+        fields = ['id', 'employee','employee_name', 'date', 'status', 'punch_in', 'punch_out', 'total_working_hours']
 
     def get_total_working_hours(self, obj):
         return obj.total_working_hours()
